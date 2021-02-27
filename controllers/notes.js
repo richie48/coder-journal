bd9ad0c37753f74a63f4bd26506ef92960165d65
richie48 <odiorrichard@gmail.com>
2021-02-27 00:19:24 +0000

    added some more queries

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,6 @@
 const Notes = require('../models/Notes');
 const asyncHandler = require('express-async-handler');
-
+const colors = require('colors');
 //desc   add note
 //access   private
 exports.addNote = asyncHandler(async (req, res, next) => {
@@ -34,7 +34,33 @@ exports.updateNote = asyncHandler(async (req, res, next) => {
 //desc   get notes
 //access   private
 exports.getNotes = asyncHandler(async (req, res, next) => {
-  const notes = await Notes.find();
+  const reqQuery = { ...req.query };
+  const removeFields = ['select', 'sort'];
+
+  removeFields.forEach((params) => delete reqQuery[params]);
+
+  let query;
+  let queryStr = JSON.stringify(reqQuery);
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Notes.find(JSON.parse(queryStr));
+
+  //creating the select feature
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    console.log(fields);
+    query = query.select(fields);
+  }
+  //creating the sorting feature
+  if (req.query.sort) {
+    const fields = req.query.sort.split(',').join(' ');
+    query = query.sort(fields);
+  }
+
+  const notes = await query;
   res.status(200).json({ success: true, data: notes });
 });
 
