602e5b10071aba350eadb24f553c12fb8b3a9228
richie48 <57833496+richie48@users.noreply.github.com>
2021-03-03 23:11:40 +0000

    added some advanced filtering

--- notes.js
+++ notes.js
@@ -31,11 +31,11 @@ exports.updateNote = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: notes });
 });
 
-//desc   get notes
+//desc   get notes and also get notes for a particular user.
 //access   private
 exports.getNotes = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   removeFields.forEach((params) => delete reqQuery[params]);
 
@@ -46,12 +46,18 @@ exports.getNotes = asyncHandler(async (req, res, next) => {
     (match) => `$${match}`
   );
 
-  query = Notes.find(JSON.parse(queryStr));
+  //The logic to get all notes for a user and the logic to just get all notes
+  if (req.params.userId) {
+    query = Notes.find({
+      user: req.params.userId,
+    });
+  } else {
+    query = Notes.find(JSON.parse(queryStr));
+  }
 
   //creating the select feature
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
-    console.log(fields);
     query = query.select(fields);
   }
   //creating the sorting feature
@@ -59,6 +65,12 @@ exports.getNotes = asyncHandler(async (req, res, next) => {
     const fields = req.query.sort.split(',').join(' ');
     query = query.sort(fields);
   }
+
+  //for pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 100;
+  const skip = (page - 1) * limit;
+  query = query.skip(skip).limit(limit);
 
   const notes = await query;
   res.status(200).json({ success: true, data: notes });
