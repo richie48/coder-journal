3641ede5c4cf1d2452508412b22d77c1f4d7f19c
richie48 <57833496+richie48@users.noreply.github.com>
2021-03-15 19:20:38 +0100

    fixed my .statics issue in notes model

---
+++ Notes.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const notesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'please enter a title'],
+      maxlength: [50, 'title cannot be more than 50 characters'],
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'please enter a description'],
+      maxlenth: [300, 'description should not be more than 300 characters'],
+    },
+    archive: {
+      type: String,
+      enum: ['archived', 'not-archived'],
+      default: 'not-archived',
+    },
+    slug: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    toJson: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+notesSchema.virtual('Notesman', {
+  ref: 'User',
+  localField: '_id',
+  foreignField: 'notes',
+  justOne: false,
+});
+
+//run this middleware before a note is saved
+notesSchema.pre('save', async function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+//Called a static method on model
+notesSchema.statics.getTotalArchived = async function (userId) {
+  const obj = await this.aggregate([
+    {
+      $match: { $and: [{ user: userId }, { archive: 'archived' }] },
+      // $match: {
+      //   user: userId,
+      //   archive: 'archived',
+      // },
+    },
+    {
+      $group: {
+        _id: null,
+        totalArchived: { $sum: 1 },
+      },
+    },
+  ]);
+  try {
+    console.log(obj);
+    await this.model('User').findByIdAndUpdate(userId, {
+      totalArchived: obj[0].totalArchived,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// this middlewares are run when a note is saved or removed.
+notesSchema.post('save', async function (next) {
+  this.constructor.getTotalArchived(this.user);
+  next();
+});
+notesSchema.post('remove', async function (next) {
+  this.constructor.getTotalArchived(this.user);
+  next();
+});
+
+module.exports = mongoose.model('Notes', notesSchema);
